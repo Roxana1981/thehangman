43d4ff1b951dedef796bf648e03d8b3ca56329b9
Roxana1981 <roxanaameijide81@gmail.com>
2023-01-03 22:41:49 +0000

    Add player guess limit and initial input validation

--- run.py
+++ run.py
@@ -7,6 +7,16 @@ print("Have fun")
 
 def hangman():
 
+    """
+    Game logic function
+    """
+
+    limit = 5
+    guess = input("Your selected word is:" + display + "Take a guess:\n")
+    guess = guess.strip()
+    if len(guess.strip()) == 0 and len(guess) != 1:
+        print("Incorrect Input, please use letter instead\n")    
+
 def replay():
 
 def main():
