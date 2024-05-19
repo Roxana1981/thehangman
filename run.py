ef0c6c691bb20197eb494ff3ac9b3a618ce5d90f
Roxana1981 <roxanaameijide81@gmail.com>
2024-05-19 10:36:04 +0000

    Update player name input section and complete heroku redployment

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 # Game message
 print ("Welcome to The Hangman game")
-player = input("Enter your name please")
+player = input("Enter your name please: ")
 print("Welcome" + player + "!" + "Let's start the game")
 print("Have fun")
 
